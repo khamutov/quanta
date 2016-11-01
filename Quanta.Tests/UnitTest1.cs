593009124ee4677ba18a3f5f70f11c7bedbde470
Aleksandr Khamutov <arekanderu.hamutobi@gmail.com>
2016-11-01 11:21:46 +0300

    initial setup

---
+++ Quanta.Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CsvHelper;
+using System.Linq;
+using System.Diagnostics;
+
+namespace Quanta.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (TextReader textReader = File.OpenText(@"C:\projects\MTLR_790101_161101.txt"))
+            {
+                var csv = new CsvReader(textReader);
+                var records = csv.GetRecords<Object>();
+
+                Trace.WriteLine(records.First());
+                Assert.AreEqual(1964, records.Count());
+            }
+        }
+    }
+}
--- Quanta/Program.cs
+++ Quanta/Program.cs
@@ -10,6 +10,8 @@ namespace Quanta
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello");
+            Console.ReadKey();
         }
     }
 }
