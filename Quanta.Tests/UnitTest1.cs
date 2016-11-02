37fe00b6c9bf7695c1972b1aadd69ee9ea22d6b3
Aleksandr Khamutov <arekanderu.hamutobi@gmail.com>
2016-11-02 11:52:40 +0300

    added stdev calculation

--- Quanta.Tests/UnitTest1.cs
+++ Quanta.Tests/UnitTest1.cs
@@ -4,6 +4,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CsvHelper;
 using System.Linq;
 using System.Diagnostics;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace Quanta.Tests
 {
@@ -16,9 +18,28 @@ namespace Quanta.Tests
             using (TextReader textReader = File.OpenText(@"C:\projects\MTLR_790101_161101.txt"))
             {
                 var csv = new CsvReader(textReader);
-                var records = csv.GetRecords<Object>();
 
-                Assert.AreEqual(1964, records.Count());
+                List<double> closePrices = new List<double>();
+                while (csv.Read())
+                {
+                    var ticker = csv.GetField<string>("<TICKER>");
+                    var period = csv.GetField<string>("<PER>");
+                    var boolField = csv.GetField<string>("<DATE>");
+                    var time = csv.GetField<string>("<TIME>");
+                    var openPrice = Double.Parse(csv.GetField<string>("<OPEN>"), CultureInfo.InvariantCulture);
+                    var highPrice = Double.Parse(csv.GetField<string>("<HIGH>"), CultureInfo.InvariantCulture);
+                    var lowPrice = Double.Parse(csv.GetField<string>("<LOW>"), CultureInfo.InvariantCulture);
+                    var closePrice = Double.Parse(csv.GetField<string>("<CLOSE>"), CultureInfo.InvariantCulture);
+                    var volume = Double.Parse(csv.GetField<string>("<VOL>"), CultureInfo.InvariantCulture);
+
+                    closePrices.Add(closePrice);
+                }
+
+                Assert.AreEqual(1964, closePrices.Count());
+
+                Assert.AreEqual(274.77, 
+                    MathNet.Numerics.Statistics.Statistics.MeanStandardDeviation(closePrices).Item1,
+                    0.1);
             }
         }
     }
--- Quanta/Program.cs
+++ Quanta/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CsvHelper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Quanta
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
