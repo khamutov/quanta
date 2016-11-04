62d0c86a149fa1bdeb4f974a966542ef0b7091a0
Aleksandr Khamutov <arekanderu.hamutobi@gmail.com>
2016-11-04 19:48:13 +0300

    added moex url for tickers download

---
+++ Quanta/DataFeed/TickerMoexDownloader.cs
@@ -0,0 +1,8 @@
+﻿namespace Quanta.DataFeed
+{
+    public class TickerMoexDownloader
+    {
+        private const string url =
+            @"http://www.moex.com/iss/apps/rms/rates.csv?iss.only=object&limit=unlimited&sort_column=SECID&sort_order=ASC&security_types=common_share,preferred_share&board_groups=stock_tplus,stock_ndm_tplus&index=&listname=1,2,3&collateral=0&currencyid=&lang=ru";
+    }
+}
