8e1d34ae09386ac8ad6a0d9ea451d7e4672b8f86
Aleksandr Khamutov <arekanderu.hamutobi@gmail.com>
2016-11-08 18:31:24 +0300

    added timeout

--- Quanta/DataFeed/WebDownloader.cs
+++ Quanta/DataFeed/WebDownloader.cs
@@ -1,8 +1,27 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 
 namespace Quanta.DataFeed
 {
     public class WebDownloader : WebClient
     {
+        public int Timeout { get; set; }
+
+        public WebDownloader()
+        {
+            Timeout = 60000;
+        }
+
+        public WebDownloader(int timeout)
+        {
+            Timeout = timeout;
+        }
+
+        protected override WebRequest GetWebRequest(Uri uri)
+        {
+            var request = base.GetWebRequest(uri);
+            request.Timeout = Timeout;
+            return request;
+        }
     }
 }
